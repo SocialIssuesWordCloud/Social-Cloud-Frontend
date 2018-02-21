c7dd9b3bb5aba71e0e4719129ef85c9f4672972a
Bryan Long <BryanLong1@gmail.com>
2018-02-21 15:55:11 -0700

    Working with JSON

--- react-word-cloud-test/src/App.js
+++ react-word-cloud-test/src/App.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react';
-import PofileCard from "./Components/Card.js"
+import ProfileCard from "./Components/Card.js"
 // import logo from './logo.svg';
 import './App.css';
 import { render } from "react-dom";
 import WordCloud from "react-d3-cloud";
 
+// Eventually the data (below) will be imported from our API
+import data from "./hello.json";
 
+// const data = [
+//   { text: " Ben", value: 100 },
+//   { text: " Sarah", value: 8300 },
+//   { text: " Bryan", value: 1000 },
+//   { text: "lol", value: 100 },
+//   { text: "first impression", value: 100 },
+//   { text: "very cool", value: 100 },
+//   { text: "duck", value: 10 },
+//   { text: "Yo Yo Yo!", value: 100 }
+// ];
 
-const data = [
-  { text: " Ben", value: 100 },
-  { text: " Sarah", value: 8300 },
-  { text: " Bryan", value: 1000 },
-  { text: "lol", value: 100 },
-  { text: "first impression", value: 100 },
-  { text: "very cool", value: 100 },
-  { text: "duck", value: 10 },
-  { text: "Bryan Testing", value: 100 }
-];
-
-const fontSizeMapper = word => Math.log2(word.value) * 5;
+const fontSizeMapper = word => Math.log2(word.value) * 6;
 const rotate = word => word.value % 360;
-
 
 class App extends Component {
   render() {
-    return (
-      <div>
-      <WordCloud data={data} fontSizeMapper={fontSizeMapper} rotate={rotate} />
-      </div>
-    );
+    const newData = data.map(item => ({ text: item.text, value: item.value }));
+    return <div>
+        <WordCloud data={newData} fontSizeMapper={fontSizeMapper} rotate={rotate} padding={6} />
+      </div>;
   }
 }
 
--- react-word-cloud-test/src/Components/Card.js
+++ react-word-cloud-test/src/Components/Card.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class PofileCard extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-        return <div>
-            <h2>Test</h2>
-            <img src="http://via.placeholder.com/350x150" alt=" Image" />
-          </div>;
-    }
+class ProfileCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2></h2>
+        <img src="http://via.placeholder.com/350x150" alt=" Image" />
+      </div>
+    );
+  }
 }
 
-export default PofileCard;
+export default ProfileCard;
