f020982e8d8f488b3fa917633c81d648db28bf4e
Bryan Long <BryanLong1@gmail.com>
2018-02-22 13:54:36 -0700

    Styled frontend

--- src/Components/WordCloud.js
+++ src/Components/WordCloud.js
@@ -5,21 +5,23 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 import data from "../hello.json";
 
-const text = styled.a`
-    display: hidden;
-    color: gray;
-`;
-
-const newData = data.map(item => ({ text: <a href={item.link}>{item.text}</a>, value: item.value }));
-const fontSizeMapper = word => Math.log2(word.value) * 4;
+const newData = data.map(item => ({
+  text: <a href={item.link}>{item.text}</a>,
+  value: item.value
+}));
+const fontSizeMapper = word => Math.log2(word.value) * 6;
 const rotate = word => word.value % 360;
 
 
 class SocialWordCloudComponent extends React.Component {
   render() {
-    return <text>
-        <WordCloud className="WordCloudDiv" data={newData} fontSizeMapper={fontSizeMapper} rotate={rotate} padding={6} />
-      </text>;
+      return<WordCloud 
+      data={newData} 
+      fontSizeMapper={fontSizeMapper} 
+      rotate={rotate} 
+      padding={6}
+      width={900}
+      height={900}/>;
   }
 }
 
