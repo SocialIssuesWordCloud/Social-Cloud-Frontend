f5a851ba3fb93a69968a054fbd207623108caa7f
lebeerman <omnidbeerman@gmail.com>
2018-02-22 15:55:36 -0700

    Makin progress on the cloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
 import React, { Component } from 'react';
-import SocialWordCloudComponent from "./Components/WordCloud.js";
+import randomColor from 'randomcolor';
+import TagCloud from 'react-tag-cloud';
+import CloudItem from './Components/CloudItem';
+import data from './hello.json';
 
-// Eventually the data (imported below) will be brought in from our API
-import './App.css';
-import Header from "./Components/Header.js"
-import SampleCard from "./Components/SampleCard.js";
-import styled from "styled-components";
-// import logo from './logo.svg';
-// import { render } from "react-dom";
+const styles = {
+  large: {
+    fontSize: 60,
+    fontWeight: 'bold'
+  },
+  small: {
+    opacity: 0.7,
+    fontSize: 16
+  }
+};
 
+const newData = data.map(item => { 
+  console.dir('thing', item);
+  return <a href={item.link}>{item.text}</a>
+ });
 
 class App extends Component {
+  componentDidMount() {
+    setInterval(() => {
+      this.forceUpdate();
+    }, 3000);
+  }
+
   render() {
-    return <div>
-        <Header />
-        <SocialWordCloudComponent />
-        <SampleCard />
-      </div>;
+    return (
+      console.log(data),
+      <div className="app-outer">
+        <div className="app-inner">
+          <h1>react-tag-cloud demo</h1>
+          <TagCloud
+            className="tag-cloud"
+            style={{
+              fontFamily: 'sans-serif',
+              fontSize: () => Math.round(Math.random() * 50) + 16,
+              fontSize: 30,
+              color: () =>
+                randomColor({
+                  hue: 'blue'
+                }),
+              padding: 5
+            }}
+          >
+
+            <div
+              style={{
+                fontFamily: 'serif',
+                fontSize: 40,
+                fontStyle: 'italic',
+                fontWeight: 'bold',
+                color: randomColor()
+              }}
+            >
+              Futurama
+            </div>
+            <CloudItem text="Custom item, Hover me!" />
+            <CloudItem text="Custom item 2, Hover me!" />
+            { newData }
+          </TagCloud>
+        </div>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
+{/* <div style={styles.large}>Transformers</div>
+            <div style={styles.large}>Simpsons</div>
+            <div style={styles.large}>Dragon Ball</div>
+            <div style={styles.large}>Rick & Morty</div>
+            <div style={{ fontFamily: 'courier' }}>He man</div>
+            <div style={{ fontSize: 30 }}>World trigger</div>
+            <div style={{ fontStyle: 'italic' }}>Avengers</div>
+            <div style={{ fontWeight: 200 }}>Family Guy</div>
+            <div style={{ color: 'green' }}>American Dad</div>
+            <div className="tag-item-wrapper">
+              <div>Hover Me Please!</div>
+              <div className="tag-item-tooltip">HOVERED!</div>
+            </div>
+            <div>Gobots</div>
+            <div>Thundercats</div>
+            <div>M.A.S.K.</div>
+            <div>GI Joe</div>
+            <div>Inspector Gadget</div>
+            <div>Bugs Bunny</div>
+            <div>Tom & Jerry</div>
+            <div>Cowboy Bebop</div>
+            <div>Evangelion</div>
+            <div>Bleach</div>
+            <div>GITS</div>
+            <div>Pokemon</div>
+            <div>She Ra</div>
+            <div>Fullmetal Alchemist</div>
+            <div>Gundam</div>
+            <div>Uni Taisen</div>
+            <div>Pinky and the Brain</div>
+            <div>Bobs Burgers</div>
+            <div style={styles.small}>Dino Riders</div>
+            <div style={styles.small}>Silverhawks</div>
+            <div style={styles.small}>Bravestar</div>
+            <div style={styles.small}>Starcom</div>
+            <div style={styles.small}>Cops</div>
+            <div style={styles.small}>Alfred J. Kwak</div>
+            <div style={styles.small}>Dr Snuggles</div> */}
