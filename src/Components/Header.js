43debb5468e7f0a0596680380c06a00b3ddd28b5
Bryan Long <BryanLong1@gmail.com>
2018-02-28 10:29:08 -0700

    Finished Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const HeaderDiv = styled.div`
   align-items: center;
   color: white;
   background-color: #325D81;
-  padding: 1rem;
+  padding: .7rem;
 `;
 const Nav = styled.div`
   display: flex;
@@ -48,21 +48,22 @@ class Header extends React.Component {
   render() {
     return <div id="top">
         <HeaderDiv>
-        <div>
-           <Nav>
-              <img src={cloud} alt="social-cloud" height="100px" />
-              <h1>Social Cloud</h1>
-           </Nav>
-        <h3>View trending social issues by location in real time</h3>
-        </div>
+          <div>
+              <a href="https://siwc-project.firebaseapp.com">
+            <Nav>
+                <img src={cloud} alt="social-cloud" height="100px" />
+                <h1>Social Cloud</h1>
+            </Nav>
+              </a>
+            <h3>View trending social issues by location in real time</h3>
+          </div>
           <Nav>
             <button id="search-button" onClick={this.toggleSubHeader}>
               Change Location <FaBeer />
             </button>
           </Nav>
         </HeaderDiv>
-        {this.state.SubHeader ? 
-          <SearchBar>
+        {this.state.SubHeader ? <SearchBar>
             <SubHeader1 citieswoeid={this.props.citieswoeid} countrywoeid={this.props.countrywoeid} worldcitieswoeid={this.props.worldcitieswoeid} personalLocations={this.props.personalLocations} searchAPILocations={this.props.searchAPILocations} searchAPIPersonalLocations={this.props.searchAPIPersonalLocations} />
           </SearchBar> : null}
       </div>;
